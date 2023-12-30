7722dc9bcf5b8247c8676f5751e1505537f69fd2
Atithiya <atithiya.inthorn@gmail.com>
2023-12-30 19:56:48 +0700

    add w-screen

--- src/App.jsx
+++ src/App.jsx
@@ -55,64 +55,66 @@ function App() {
   };
 
   return (
-    <div className="max-w-5xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-white">
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-        {/* {poke?.name} จะแสดงค่า name ของ poke ถ้า poke มีค่า (ไม่ใช่ null หรือ undefined) และถ้า poke ไม่ได้กำหนดค่าหรือมีค่าเป็น null หรือ undefined จะแสดงค่าว่าง */}
-        <div>
-          {loading ? (
-            <ReactLoading
-              type="spin"
-              color="black"
-              height={"20%"}
-              width={"20%"}
-            />
-          ) : (
-            <>
-              <h1 className="text-5xl">{poke?.name}</h1>
-              <button
-                onClick={addFav}
-                className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-5"
-              >
-                Add to favourite
-              </button>
-              <div>
+    <div className="h-screen flex justify-center items-center">
+      <div className="max-w-5xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-white ">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+          {/* {poke?.name} จะแสดงค่า name ของ poke ถ้า poke มีค่า (ไม่ใช่ null หรือ undefined) และถ้า poke ไม่ได้กำหนดค่าหรือมีค่าเป็น null หรือ undefined จะแสดงค่าว่าง */}
+          <div>
+            {loading ? (
+              <ReactLoading
+                type="spin"
+                color="black"
+                height={"20%"}
+                width={"20%"}
+              />
+            ) : (
+              <>
+                <h1 className="text-5xl">{poke?.name}</h1>
                 <button
-                  onClick={prevPoke}
-                  className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  onClick={addFav}
+                  className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-5"
                 >
-                  Previous
+                  Add to favourite
                 </button>
-                <button
-                  onClick={nextPoke}
-                  className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                >
-                  Next
-                </button>
-                <img
-                  src={poke?.sprites?.other?.home?.front_default}
-                  alt={poke?.name}
-                  className="w-full h-[400px]"
-                />
-              </div>
-              <div>
-                <p className="font-bold text-cyan-500">Skills</p>
-                <ul>
-                  {poke?.abilities?.map((abil, idx) => (
-                    <li key={idx}>{abil?.ability.name}</li>
-                  ))}
-                </ul>
-              </div>
-            </>
-          )}
-        </div>
-        <div>
-          <h2 className="text-2xl">Your favourite Pokemon</h2>
+                <div>
+                  <button
+                    onClick={prevPoke}
+                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  >
+                    Previous
+                  </button>
+                  <button
+                    onClick={nextPoke}
+                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  >
+                    Next
+                  </button>
+                  <img
+                    src={poke?.sprites?.other?.home?.front_default}
+                    alt={poke?.name}
+                    className="w-full h-[400px]"
+                  />
+                </div>
+                <div>
+                  <p className="font-bold text-cyan-500">Skills</p>
+                  <ul>
+                    {poke?.abilities?.map((abil, idx) => (
+                      <li key={idx}>{abil?.ability.name}</li>
+                    ))}
+                  </ul>
+                </div>
+              </>
+            )}
+          </div>
+          <div>
+            <h2 className="text-2xl">Your favourite Pokemon</h2>
 
-          {fav.length > 0 ? (
-            <FavPoke fav={fav} />
-          ) : (
-            <p className="mt-5">No favourite Pokemon.</p>
-          )}
+            {fav.length > 0 ? (
+              <FavPoke fav={fav} />
+            ) : (
+              <p className="mt-5">No favourite Pokemon.</p>
+            )}
+          </div>
         </div>
       </div>
     </div>
